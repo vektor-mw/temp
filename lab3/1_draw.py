aee154f0c105f3371c60083c150ba08b559cb903
Ivoilovskiy Mikhail <vektor-mw@yandex.ru>
2020-10-02 00:23:18 +0300

    злобный смайлик2

--- lab3/1_draw.py
+++ lab3/1_draw.py
@@ -1,27 +1,28 @@
-
 import pygame
 from pygame.draw import *
 
-# После импорта библиотеки, необходимо её инициализировать:
 pygame.init()
 
-# И создать окно:
-screen = pygame.display.set_mode((300, 200))
+FPS = 30
+screen = pygame.display.set_mode((400, 400))
 
-# здесь будут рисоваться фигуры
-# ...
+rect(screen, (255, 0, 255), (100, 100, 200, 200))
+rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
+polygon(screen, (255, 255, 0), [(100,100), (200,50),
+                               (300,100), (100,100)])
+polygon(screen, (0, 0, 255), [(100,100), (200,50),
+                               (300,100), (100,100)], 5)
+circle(screen, (0, 255, 0), (200, 175), 50)
+circle(screen, (255, 255, 255), (200, 175), 50, 5)
 
-# после чего, чтобы они отобразились на экране, экран нужно обновить:
 pygame.display.update()
-# Эту же команду нужно будет повторять, если на экране происходят изменения.
-
-# Наконец, нужно создать основной цикл, в котором будут отслеживаться
-# происходящие события.
-# Пока единственное событие, которое нас интересует - выход из программы.
 clock = pygame.time.Clock()
+finished = False
 
-while True:
+while not finished:
+    clock.tick(FPS)
     for event in pygame.event.get():
-        clock.tick(30)
         if event.type == pygame.QUIT:
-            pygame.quit()
+            finished = True
+
+pygame.quit()
--- lab3/3_1_1_draw.py
+++ lab3/3_1_1_draw.py
@@ -1,19 +1,43 @@
 import pygame
 from pygame.draw import *
+import numpy as np
 
 pygame.init()
+
+
+def rec_angle(x1, y1, a, b, angle):
+    """Вычисляет координаты вершин наклонного прямоугольника
+
+    :param x1: координата x начальной точки
+    :param y1: координата y начальной точки
+    :param a: длина прямоугольника
+    :param b: ширина прямоугольника
+    :param angle: угол наклона
+    :return: список кортежей координат вершин прямоугольника
+    """
+    angle = angle * np.pi / 180
+    x2 = x1 + a * np.cos(angle)
+    y2 = y1 + a * np.sin(angle)
+    x3 = x1 + a * np.cos(angle) - b * np.sin(angle)
+    y3 = y1 + a * np.sin(angle) + b * np.cos(angle)
+    x4 = x1 - b * np.sin(angle)
+    y4 = y1 + b * np.cos(angle)
+    A = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+    return A
+
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+rect(screen, (150, 150, 150), (0, 0, 400, 400), 0)
+circle(screen, (255, 255, 0), (200, 175), 100, 0)
+rect(screen, (0, 0, 0), (145, 220, 110, 25), 0)
+circle(screen, (255, 0, 0), (160, 140), 20, 0)
+circle(screen, (0, 0, 0), (160, 140), 10, 0)
+circle(screen, (255, 0, 0), (250, 145), 18, 0)
+circle(screen, (0, 0, 0), (250, 145), 8, 0)
+polygon(screen, (0, 0, 0), rec_angle(120, 78, 90, 8, 35))
+polygon(screen, (0, 0, 0), rec_angle(302, 95, 80, 8, 150))
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -26,3 +50,4 @@ while not finished:
             finished = True
 
 pygame.quit()
+
